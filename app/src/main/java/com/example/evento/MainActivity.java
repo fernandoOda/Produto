673066074881f4a68684b4e8b9dfe5d13c56a672
nando <70904605+fernandoOda@users.noreply.github.com>
2020-10-18 16:52:55 -0300

    Commit 2

--- app/src/main/java/com/example/evento/MainActivity.java
+++ app/src/main/java/com/example/evento/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     public void onClickNew(View v) {
         Intent intent = new Intent(MainActivity.this, CadastroEvento.class);
         startActivityForResult(intent, requestCodeNovoEvento);
